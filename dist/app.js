cf305473ae292b81fe4f0d62c0df0437ee06be57
Olesia Kokhman <olesia.kokhman.pz.2021@lpnu.ua>
2024-05-28 18:06:45 +0300

    connect to mongodb

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,36 @@
 "use strict";
-console.log("hello world in node.js using typescript");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://olesia:somepassword@bank-project.r6mfekg.mongodb.net/?retryWrites=true&w=majority&appName=bank-project";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            // Connect the client to the server	(optional starting in v4.7)
+            yield client.connect();
+            // Send a ping to confirm a successful connection
+            yield client.db("admin").command({ ping: 1 });
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        }
+        finally {
+            // Ensures that the client will close when you finish/error
+            yield client.close();
+        }
+    });
+}
+run().catch(console.dir);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log("hello world in node.js using typescript")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://olesia:somepassword@bank-project.r6mfekg.mongodb.net/?retryWrites=true&w=majority&appName=bank-project";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
